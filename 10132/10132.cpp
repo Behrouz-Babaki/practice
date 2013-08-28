a4a95d624ddd62427d6a55692a78eeea74ab68d0
Behrouz Babaki <BehrouzEslamiBabaki@cs.kuleuven.be>
2013-08-28 02:57:37 +0200

    implementing function createOriginalFile

--- 10132/10132.cpp
+++ 10132/10132.cpp
@@ -17,34 +17,56 @@ using std::bitset;
 void createOriginalFile (const vector<vector<bool > > &);
 bool testConcat (const vector<vector<bool > >&, int, int, const vector<bool> &);
 bool checkNode (const vector<vector<bool> >&, const vector<vector<int> >&, vector<int>&, const vector<bool>&);
+void findPossibleMatches (const vector<vector<bool> >&, vector<vector<int> >);
 
 int main (void){
   int number_of_cases, bla;
   cin >> number_of_cases >> ws;
   
   for (int counter = 0; counter < number_of_cases; counter++)
-    {
-      vector<vector<bool> > fragments;
-      string line;
-      getline(cin, line);
+  {
+    vector<vector<bool> > fragments;
+    string line;
+    getline(cin, line);
 
-      while (!line.empty()){
-	vector<bool> fragment;
-	for (string::iterator itr = line.begin(), endItr = line.end(); itr != endItr; itr++)
-	  fragment.push_back(((*itr) == '1' ? true : false));
+    while (!line.empty()){
+      vector<bool> fragment;
+      for (string::iterator itr = line.begin(), endItr = line.end(); itr != endItr; itr++)
+        fragment.push_back(((*itr) == '1' ? true : false));
 
-	fragments.push_back(fragment);
-	getline (cin, line);
-      }
-      
-      createOriginalFile (fragments);
+      fragments.push_back(fragment);
+      getline (cin, line);
     }
+      
+    createOriginalFile (fragments);
+  }
   
   return 0;
 }
 
 void createOriginalFile (const vector<vector<bool> > & fragments){
-  //TODO write this function
+  /// find the possible matches
+  vector<vector<int> > possible_matches;
+  findPossibleMatches(fragments, possible_matches);
+  
+  /// for all possible matches, call checkNode
+  for (int counter = 0; counter < fragments.size(); counter++)
+    for (vector<int>::iterator itr = possible_matches[counter].begin(), itrEnd = possible_matches[counter].end(); itr != itrEnd; itr++)
+    {
+      vector<bool> file;
+      for (vector<bool>::const_iterator firstItr = fragments[counter].begin(), firstEnd = fragments[counter].end(); firstItr != firstEnd; firstItr++)
+        file.push_back(*firstItr);
+      for (vector<bool>::const_iterator secondItr = fragments[*itr].begin(), secondEnd = fragments[*itr].end(); secondItr != secondEnd; secondItr++)
+        file.push_back(*secondItr);
+
+      vector<int> matched (fragments.size(), -1);
+      matched[counter] = *itr;
+      matched[*itr] = counter;
+      if (checkNode (fragments, possible_matches, matched, file))
+        for (vector<bool>::iterator fileItr = file.begin(), fileItrEnd = file.end(); fileItr != fileItrEnd; fileItr++)
+          cout << (*fileItr ? '1' : '0') << endl;
+    }
+  /// TODO (improvement) keep track of failed matches
 }
 
 /** 
@@ -71,25 +93,26 @@ bool checkNode (const vector<vector<bool> >& fragments, const vector<vector<int>
   if (unmatched.empty()) 
     return true;
 
-
   ///  for any of the unmatched fragments
   for (vector<int>::iterator unmatched_itr = unmatched.begin(), end = unmatched.end(); unmatched_itr != end; unmatched_itr++)
-
-  // for any of the possible matches for that fragment (which are not already matched)
+    
+    /// for any of the possible matches for that fragment (which are not already matched) 
     for (vector<int>::const_iterator matches_itr = possible_matches[*unmatched_itr].begin() , m_itr_end = possible_matches[*unmatched_itr].end(); matches_itr != m_itr_end; matches_itr++)
       if (matched[*matches_itr] == -1)
-  // if the match works for any of the two directions (concatenation to right/left)
+
+        /// if the match works for any of the two directions (concatenation to right/left)
+
         if (testConcat (fragments, *matches_itr, *unmatched_itr, file))
-  // mark the two as matched
+          /// mark the two as matched
         {
           matched[*unmatched_itr] = *matches_itr;
           matched[*matches_itr] = *unmatched_itr;
-  // call the function again
-          if (checkNode(fragments, possible_matches, matched, file))
-            return true;
-  // erase the marks
-          matched[*unmatched_itr] = -1;
-          matched[*matches_itr] = -1;
+          /// call the function again
+            if (checkNode(fragments, possible_matches, matched, file))
+              return true;
+            /// erase the marks
+            matched[*unmatched_itr] = -1;
+            matched[*matches_itr] = -1;
         }
 
   return false;
@@ -110,4 +133,10 @@ bool checkNode (const vector<vector<bool> >& fragments, const vector<vector<int>
 bool testConcat (const vector<vector<bool > >& fragments, int index1, int index2, const vector<bool> & file){
   //TODO write this function
   return false;
+}
+
+
+void findPossibleMatches (const vector<vector<bool> >& fragments, vector<vector<int> > possible_matches){
+  /// TODO implement the function
+  return;
 }
