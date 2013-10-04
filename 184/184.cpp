09e5ebbf2845d33ed4a2732d99416b442c56e49f
Behrouz Babaki <Behrouz.EslamiBabaki@cs.kuleuven.be>
2013-10-04 17:13:37 +0200

    still needs correction

--- 184/184.cpp
+++ 184/184.cpp
@@ -44,8 +44,6 @@ struct lineCompare{
 
 typedef set<set<pair<unsigned int, unsigned int>,pairCompare >,lineCompare > lineSet;
 
-
-void printSet(const pairSet&);
 void printResults (const pairSet&);
 void findLines (const pairSet &, lineSet&);
 
@@ -85,11 +83,15 @@ void printResults (const pairSet &instanceData)
   lineSet lines;
 
     findLines(instanceData, lines);
-    cout << "number of lines:" << lines.size() << endl;
+    if (lines.empty())
+      cout << "No lines were found" << endl;
+    else {
+      cout << "The following lines were found:" << endl;
     for (auto lineItr = lines.begin(), lineItrEnd = lines.end(); lineItr != lineItrEnd; lineItr++){
       for (auto pointItr = lineItr->begin(), pointItrEnd = lineItr->end(); pointItr != pointItrEnd; pointItr++)
 	cout << "(" << setw(4) << pointItr->first << "," << setw(4) << pointItr->second << ")";
       cout << endl;
+    }
     }
     
   return;
@@ -112,12 +114,6 @@ void findLines (const pairSet &instanceData,  lineSet &lines){
 	  {
 	    if (candItr == firstPairItr || candItr == secondPairItr)
 	      continue;
-	    
-	    // /******************************/
-	    // cout << "(" << firstPairItr->first << "," << firstPairItr->second << ")"
-	    // 	 << " (" << secondPairItr->first << "," << secondPairItr->second << ")"
-	    // 	 << " (" << candItr->first << "," << candItr->second << ")\t";
-	    // /******************************/
 
 	    if ((firstPairItr->first-secondPairItr->first)*(firstPairItr->second - candItr->second) == (firstPairItr->first - candItr->first)*(firstPairItr->second - secondPairItr->second))
 	      current_line.insert(*candItr);
@@ -125,18 +121,11 @@ void findLines (const pairSet &instanceData,  lineSet &lines){
 	  }
 
 	if (current_line.size() > 2)
-	  {
-	    cout << "found a line" << endl;
 	    lines.insert(current_line);
-	  }
+
       }
   }
-  cout << "lines found: " << lines.size() << endl;
+
   return;
 }
 
-void printSet(const pairSet& points){
-  for (auto itr = points.begin(), endItr = points.end(); itr != endItr; itr++)
-    cout << "(" << itr->first << "," << itr->second << ")\t";
-  cout << endl;
-}
