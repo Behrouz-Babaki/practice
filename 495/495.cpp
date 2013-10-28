29d55a169d7e952c46d235621d9bb1ec7a86d18b
Behrouz Babaki <Behrouz.EslamiBabaki@cs.kuleuven.be>
2013-10-28 02:46:32 +0100

    adding big number representation

--- 495/495.cpp
+++ 495/495.cpp
@@ -1,19 +1,86 @@
 #include <iostream>
+#include <vector>
+#include <stack>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::vector;
+using std::stack;
+
+#define MAX 5000
+
+void sumUp(const vector<unsigned char>&, const vector<unsigned char>&, vector<unsigned char>&);
 
 int main(void){
-  unsigned long int a[5001];
-  a[0] = 0;
-  a[1] = 1;
-  for (int counter = 2; counter <= 5000; counter++)
-    a[counter] = a[counter - 1] + a[counter - 2];
+  vector<vector<unsigned char> > nums;
 
-  int num;
-  while (cin >> num)
-    cout << "The Fibonacci number for " << num << " is " << a[num] << endl;
+  vector<unsigned char> zero;
+  zero.push_back(0);
+  nums.push_back(zero);
+
+  vector<unsigned char> one;
+  one.push_back(1);
+  nums.push_back(one);
+
+  for (int counter = 2; counter <= MAX; counter++){
+      vector<unsigned char> next;
+      sumUp(nums[counter - 1], nums[counter - 2], next);
+      nums.push_back(next);
+    }
+
+  int val;
+  while (cin >> val){
+    vector<unsigned char>& vec = nums[val];
+    cout << "The Fibonacci number for " << val << " is ";
+    for (vector<unsigned char>::iterator itr = vec.begin(), endItr = vec.end(); itr != endItr; itr++)
+      cout << (unsigned int) (*itr);
+    cout << endl;
+  }
 
   return 0;
+}
+
+
+void sumUp(const vector<unsigned char>& first, const vector<unsigned char>& second, vector<unsigned char>& result){
+
+  result.clear();
+  stack<unsigned char> res;
+  unsigned char carry = 0;
+  vector<unsigned char>::const_reverse_iterator itr1 = first.rbegin(), itr2 = second.rbegin(), itr1End = first.rend(), itr2End = second.rend();
+  for ( ;itr1 != itr1End && itr2 != itr2End; itr1++, itr2++){
+    unsigned char dig = *itr1 + *itr2 + carry;
+    res.push(dig % 10);
+    carry = dig / 10;
+  }
+  vector<unsigned char>::const_reverse_iterator itr, itrEnd;
+  bool remaining = false;
+  if (itr1 != itr1End){
+    itr = itr1;
+    itrEnd = itr1End;
+    remaining = true;
+  }
+  if (itr2 != itr2End){
+    itr = itr2;
+    itrEnd = itr2End;
+    remaining = true;
+  }
+  if (remaining)
+    for (; itr!= itrEnd; itr++){
+      unsigned char dig = *itr + carry;
+      res.push(dig % 10);
+      carry = dig / 10;
+  }
+
+  while (carry > 0){
+    res.push(carry % 10);
+    carry /= 10;
+  }
+
+  result.reserve(res.size());
+  while (!res.empty()){
+    result.push_back(res.top());
+    res.pop();
+  }
+    
 }
