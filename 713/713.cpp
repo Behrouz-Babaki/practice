78c3ccfed0fa991f692982fb57ea7973952aa9fd
Behrouz Babaki <Behrouz.EslamiBabaki@cs.kuleuven.be>
2013-11-03 14:44:24 +0100

    fixing one of the bugs

--- 713/713.cpp
+++ 713/713.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <stack>
+#include <cctype>
 
 using std::cout;
 using std::endl;
 using std::cin;
 using std::string;
 using std::vector;
-using std::stack;
+using std::ws;
 
 int main(void){
   int n;
-  cin >> n;
+  cin >> n >> ws;
   for (int counter = 0; counter < n; counter++)
     {
+      cin >> ws;
       string line;
       getline(cin,line);
       vector<unsigned int> first, second;
       
-      string::iterator itr, endItr;
-      for (itr = line.begin(); *itr != ' '; itr++)
+      string::iterator itr= line.begin(), endItr = line.end();
+      for (; !isspace(*itr); itr++)
 	first.push_back((unsigned int) (*itr) - 48);
-
+      
+      while (isspace(*itr))
       itr ++;
 
-      for (endItr = line.end(); itr != endItr; itr++)
+      for (; itr != endItr; itr++)
 	second.push_back((unsigned int) (*itr) - 48);
-      
+
       unsigned int carry = 0;
       vector<unsigned int>::iterator firstItr , firstItrEnd, secondItr, secondItrEnd;
       firstItr = first.begin();
@@ -35,7 +37,7 @@ int main(void){
       firstItrEnd = first.end();
       secondItrEnd = second.end();
       
-      stack<unsigned int> result;
+      vector<unsigned int> result;
       while (true){
 	if (firstItr == firstItrEnd && secondItr == secondItrEnd)
 	  break;
@@ -45,24 +47,18 @@ int main(void){
 	if (secondItr != secondItrEnd)
 	  sum += (*(secondItr++));
 	
-	result.push(sum%10);
+	result.push_back(sum%10);
 	carry = sum / 10;
       }
 
       while (carry > 0){
-	result.push(carry % 10);
+	result.push_back(carry % 10);
 	carry /= 10;
       }
 
-      if (result.empty())
-	cout << "Are you kidding me or what?" << endl;
-      while(result.top() == 0)
-	result.pop();
-
-      while (!result.empty()){
-	cout << result.top();
-	result.pop();
-      }
+      for (vector<unsigned int>::iterator itr = result.begin(), endItr = result.end(); itr != endItr; itr++)
+	if ((*itr) != 0)
+	  cout << *itr;
       cout << endl;
     }
   return 0;
