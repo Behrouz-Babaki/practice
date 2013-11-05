1580e82c9717626263bcc2644d3f1b74e7b8edbc
Behrouz Babaki <Behrouz.EslamiBabaki@cs.kuleuven.be>
2013-11-05 15:57:13 +0100

    still not working. requires big number operations even for the sum of digits

--- 10070/10070.cpp
+++ 10070/10070.cpp
@@ -1,21 +1,64 @@
 #include <iostream>
+#include <string>
 using std::cout;
 using std::endl;
 using std::cin;
-
-/* Solving this requires using big numbers */
+using std::string;
 
 int main(void){
-  unsigned long int year;
-  while (cin >> year){
+  string line;
+  while (getline(cin,line)){
+    bool divByFour = false, divByHundred = false, divByEleven = false, divByFive = false, divByThree = false, divByFH = false;
+    int oddSum = 0;
+    int evenSum = 0;
+    bool isOdd = true;
+    for (string::iterator itr = line.begin(), endItr = line.end(); itr != endItr; itr++, isOdd = !isOdd)
+      {
+	int digit = (int)(*itr) - 48;
+	if (isOdd)
+	  oddSum += digit;
+	else 
+	  evenSum += digit;
+      }
+    
+    int sumDiff = oddSum - evenSum;
+    if (sumDiff == 0 || sumDiff % 11 == 0)
+      divByEleven = true;
+
+    int sum = evenSum + oddSum;
+    if (sum % 3 == 0)
+      divByThree = true;
+
+    int last = (int)(line.at(line.length() - 1) - 48);
+    int prev = (int)(line.at(line.length() - 2) - 48);
+    int threeToEnd = (int)(line.at(line.length() - 3) - 48);
+    int fourToEnd = (int)(line.at(line.length() - 4) - 48);
+
+    int lastTwo = prev * 10 + last;
+      
+    if (lastTwo % 4 == 0)
+      divByFour = true;
+
+    if (last == 0 || last == 5)
+      divByFive = true;
+
+    if (lastTwo == 0){
+      divByHundred = true;
+
+      int n =  fourToEnd * 10 + threeToEnd;
+      if (n % 4 == 0)
+	divByFH = true;
+    }
+
+
     bool leap = false, huluculu = false, bulukulu = false;
-    if (year % 4 == 0 && year % 100 != 0)
+    if (divByFour && !divByHundred)
       leap = true;
-    if (year % 400 == 0)
+    if (divByFH)
       leap = true;
-    if (year % 15 == 0)
+    if (divByThree && divByFive)
       huluculu = true;
-    if (leap && year % 55 == 0)
+    if (leap && divByFive && divByEleven)
       bulukulu = true;
     
     if (leap)
