8b43dd5806b71597e055dd7ed66cda5ebd5f9a53
bhrzslm <Behrouz.Babaki@cs.kuleuven.be>
2013-11-09 18:01:00 +0100

    a C version - still not working

---
+++ 10190/__C/10190.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void main(void){
+   unsigned int first, second;
+   while (scanf("%d %d" ,&first, &second) != EOF){
+      int flag = 1;
+      if (first < 2 || second < 2)
+	 flag = 0;
+      else if (first % second != 0)
+	 flag = 0;
+
+      unsigned int sequence [100];
+      unsigned int index = 0;
+      while (flag && first > 1){
+	 sequence[index++] = first;
+	 if (first % second != 0)
+	    flag = 0;
+	 first /= second;
+	 if (first >= sequence[index - 1])
+	    flag = 0;
+      }
+
+      if (!flag)
+	 printf("Boring!\n");
+      else{
+	 int counter;
+	 for (counter = 0; counter < index; counter++)
+	    printf("%d ", sequence[counter]);
+	 printf("%d\n" , first);
+      }
+   }
+}
