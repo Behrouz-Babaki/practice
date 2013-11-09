1d544cd313722b477ea414bc01c632e88c1e7d26
bhrzslm <Behrouz.Babaki@cs.kuleuven.be>
2013-11-09 19:33:51 +0100

    still not working

--- 10190/__C/10190.c
+++ 10190/__C/10190.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 
 void main(void){
-   unsigned int first, second;
-   while (scanf("%d %d" ,&first, &second) != EOF){
-      int flag = 1;
-      if (first < 2 || second < 2)
-	 flag = 0;
-      else if (first % second != 0)
-	 flag = 0;
+  unsigned int first, second;
+  while (scanf("%u %u" ,&first, &second) != EOF){
+    int flag = 1;
+    if (first < 2 || second < 2)
+      flag = 0;
+    else if (first % second != 0)
+      flag = 0;
 
-      unsigned int sequence [100];
-      unsigned int index = 0;
-      while (flag && first > 1){
-	 sequence[index++] = first;
-	 if (first % second != 0)
-	    flag = 0;
-	 first /= second;
-	 if (first >= sequence[index - 1])
-	    flag = 0;
-      }
+    unsigned int sequence [100];
+    unsigned int index = 0;
+    while (flag && (first > 1)){
+      sequence[index++] = first;
+      if (first % second != 0)
+	flag = 0;
+      first /= second;
+      if (first >= sequence[index - 1])
+	flag = 0;
+    }
 
-      if (!flag)
-	 printf("Boring!\n");
-      else{
-	 int counter;
-	 for (counter = 0; counter < index; counter++)
-	    printf("%d ", sequence[counter]);
-	 printf("%d\n" , first);
-      }
-   }
+    if (!flag)
+      printf("Boring!\n");
+    else{
+      int counter;
+      for (counter = 0; counter < index; counter++)
+	printf("%u ", sequence[counter]);
+      printf("%u\n" , first);
+    }
+  }
 }
