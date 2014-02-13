59d12aa50881d8943ac7084052bc60d6d3a2e8d0
bhrzslm <Behrouz.Babaki@cs.kuleuven.be>
2014-02-13 22:57:01 +0100

    problem 124 started

---
+++ 124/124.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file   124.cpp
+ * @author Behrouz Babaki
+ * @date   Thu Feb 13 22:49:29 2014
+ * 
+ * @brief  UVA onlineJudge problem no. 124
+ *
+ * Given a list of variable constraints of the form x < y,
+ * you are to write a program that prints all orderings of the
+ * variables that are consistent with the constraints.
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::istringstream;
+
+int main(void){
+  cout << "Hello World!" << endl;
+  cout << "124" << endl;
+  return 0;
+}
