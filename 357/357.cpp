65e2c4c7cc8ab2e6cce2057d0927dd3a25eec36e
Behrouz Babaki <Behrouz.EslamiBabaki@cs.kuleuven.be>
2014-03-13 20:49:32 +0100

    357 Dynamic Programming Solution

--- 357/357.cpp
+++ 357/357.cpp
@@ -3,48 +3,39 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-long long int ways(unsigned int, unsigned int);
-
-long long int w[30001][5];
-int coins[] = {50, 25, 10, 5, 1};
-const int coinTypes = 5;
+const unsigned int maxChange = 30000;
+long long int w[maxChange+1][5];
+unsigned int coins[] = {50, 25, 10, 5, 1};
+const unsigned int coinTypesCount = 5;
 
 int main(void){
-
-  for (int counter = 0; counter < 30001; counter++)
-    for (int coinCounter = 0; coinCounter < 5; coinCounter++)
-      w[counter][coinCounter] = -1;
   
+  /* There is only 1 way to generate 0 using any types of coins */
+  for (unsigned int coinCounter = 0; coinCounter < coinTypesCount; coinCounter++)
+    w[0][coinCounter] = 1;
+  
+  /* For every value of change, compute the number of ways that this
+     value could be generated using coins no larger than a limit. Store
+     this count in w[value][limit]. */
+  for (unsigned int counter = 1; counter < maxChange + 1; counter++)
+    for (unsigned int coinCounter = 0; coinCounter < coinTypesCount; coinCounter++){
+      w[counter][coinCounter] = 0;
+      for (unsigned int innerCounter = coinCounter; innerCounter < coinTypesCount; innerCounter++)
+	if (counter >= coins[innerCounter])
+	  w[counter][coinCounter] += w[counter-coins[innerCounter]][innerCounter];
+    }
+
   unsigned int val;
   
   while (cin >> val){
-
-    long long int waysCount = ways(val, 0);
-    if (waysCount > 1)
-      cout << "There are " << waysCount << " ways";
+    if (w[val][0] > 1)
+      cout << "There are " << w[val][0] << " ways";
     else
       cout << "There is only 1 way";
     cout << " to produce " << val << " cents change." << endl;
-
   }
 
   return 0;
 }
 
-
-long long int ways(unsigned int val, unsigned int boundIndex){
-  if (!val)
-    return 1;
-
-  if (w[val][boundIndex] >= 0)
-    return w[val][boundIndex];
-
-  long long int waysCount = 0;
-  for (unsigned int boundIdCounter = boundIndex; boundIdCounter < coinTypes; boundIdCounter++)
-    if (val >= coins[boundIdCounter])
-      waysCount += ways(val - coins[boundIdCounter], boundIdCounter);
-  
-  w[val][boundIndex] = waysCount;
-  return waysCount;
-}
 
