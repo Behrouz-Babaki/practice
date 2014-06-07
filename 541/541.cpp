6a1e0c7acab1199eed6b456b0badb0a78a62712e
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-06-07 17:34:37 +0200

    computing the sums without storing the matrix

--- 541/541.cpp
+++ 541/541.cpp
@@ -6,21 +6,49 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-void checkMatrix(const vector<vector<int> >&);
-
 int main(void){
   int n = -1;
   int currentNum = -1;
   cin >> n;
   while (n){
-    vector<vector<int> > matrix(n, vector<int>(n,0));
+
+    vector<int> colSum (n, 0);
+    vector<int> rowSum (n, 0);
+
+
     for (int lineCounter = 0; lineCounter < n; lineCounter++)
       for (int numCounter = 0; numCounter < n; numCounter++) {
 	cin >> currentNum;
-	matrix[lineCounter][numCounter] = currentNum;
-	}
+	rowSum[lineCounter] += currentNum;
+	colSum[numCounter] += currentNum;
+      }
+    
+    bool tooMany = false;
+    int oddRow = -1;
+    int oddColumn = -1;
+    for (int counter = 0; !tooMany && counter < n; counter++){
+      if (rowSum[counter] % 2){
+	if (oddRow == -1)
+	  oddRow = counter;
+	else
+	  tooMany = true;
+      }
 
-    checkMatrix(matrix);
+      if (colSum[counter] % 2){
+	if (oddColumn == -1)
+	  oddColumn = counter;
+	else
+	  tooMany = true;
+      }
+    }
+    
+    if (oddRow == -1 && oddColumn == -1)
+      cout << "OK" << endl;
+    else if (tooMany)
+      cout << "Corrupt" << endl;
+    else
+      cout << "Change bit (" << oddRow + 1 << "," << oddColumn + 1 << ")" << endl;
+
     cin >> n;
   }
 
@@ -28,39 +56,3 @@ int main(void){
   return 0;
 }
 
-void checkMatrix(const vector<vector<int> >& matrix){
-  int size = matrix.size();
-  vector<int> oddRows;
-  vector<int> oddCols;
-
-  for (int row = 0; row < size; row++)
-    {
-      int sum = 0;
-      for (int col = 0; col < size; col++)
-	sum += matrix[row][col];
-      if (sum % 2)
-	oddRows.push_back(row);
-    }
-
-  for (int col = 0; col < size; col++)
-    {
-      int sum = 0;
-      for (int row = 0; row < size; row++)
-	sum += matrix[row][col];
-      if (sum % 2)
-	oddCols.push_back(col);
-    }
-
-  if (oddRows.empty() && oddCols.empty()){
-    cout << "OK" << endl;
-    return;
-  }
-
-  if (oddRows.size() == 1 && oddCols.size() == 1){
-    cout << "Change bit (" << oddRows.front() + 1 << "," << oddCols.front() + 1 << ")" << endl;
-    return;
-  }
-
-  cout << "Corrupt" << endl;
-  return;
-}
