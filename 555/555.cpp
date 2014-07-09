8bb6c5364599daca5a4029aa2b7dffc0aec8722e
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-07-09 16:59:41 +0200

    555 accepted

--- 555/555.cpp
+++ 555/555.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <utility>
 #include <map>
+#include <vector>
+#include <algorithm>
 
 using std::cin;
 using std::cout;
@@ -10,6 +12,10 @@ using std::string;
 using std::map;
 using std::pair;
 using std::make_pair;
+using std::vector;
+using std::sort;
+
+typedef pair<int,int> card;
 
 int main(void){
 
@@ -36,7 +42,32 @@ int main(void){
     cin >> first >> second;
     first.append(second);
 
+    vector<vector<card> > suits(4);
+    int pInd = playerMap.at(c);
+
+    for (int counter=0; counter<52; counter++){
+      pInd = (pInd + 1) % 4;
+      int color = colorMap.at(first.at(counter*2));
+      int card = cardMap.at(first.at(counter*2+1));
+      suits[pInd].push_back(make_pair(color,card));
+    }
     
+    for (int counter=0; counter<4; counter++){
+      sort(suits[counter].begin(), suits[counter].end(),
+	   [](const card& left, const card& right){
+	       if (left.first == right.first)
+		 return left.second < right.second;
+	       return left.first < right.first;
+	     });
+      cout << players[counter] << ": ";
+      for (auto x:suits[counter]){
+	cout << colors[x.first] << cards[x.second];
+	if (x != suits[counter].back())
+	  cout << " ";
+      }
+      cout << endl;
+    }
+
     cin >> c;
   }
   
