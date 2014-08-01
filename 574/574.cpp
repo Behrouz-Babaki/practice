1868b2f26a893ef9bb037ac17815ce4072010f1a
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-08-01 16:46:07 +0200

    574 accepted

--- 574/574.cpp
+++ 574/574.cpp
@@ -4,6 +4,7 @@
 
 using std::cout;
 using std::cin;
+using std::flush;
 using std::endl;
 using std::vector;
 using std::pair;
@@ -11,22 +12,53 @@ using std::make_pair;
 
 vector<pair<int,int> > input;
 vector<int> maxLeft;
+int t;
+int n;
+int numWays;
 
-void printSums (int, int, const vector<int>&);
+void search (int, vector<int>&);
+void printComponents (const vector<int>&);
+
 int main (void) {
-  int t, n;
+
   cin >> t;
   cin >> n;
 
   while (n != 0) {
+
+    numWays = 0;
+    input.clear();
+    maxLeft.clear();
+
     vector<int> x(n);
     for (int counter =0; counter < n; counter++)
       cin >> x[counter];
 
-    initializeGlobals();
+    int prev = x[0];
+    int reps = 1;
+    for (int counter = 1; counter < x.size(); counter++) {
+      if (x[counter] == prev)
+	reps ++;
+      else {
+	input.push_back(make_pair (prev, reps));
+	reps = 1;
+      }
+      prev = x[counter];
+    }
+    input.push_back(make_pair (prev, reps));
+
+    maxLeft.resize(input.size());
+    int remaining = 0;
+    for (int counter = input.size() - 1; counter >= 0; counter--){
+      maxLeft[counter] = remaining + input[counter].first * input[counter].second;
+      remaining = maxLeft[counter];
+    }
     
     cout << "Sums of " << t << ":" << endl;
-    printSums (t, n, x);
+    vector<int> selected;
+    search (0, selected);
+    if (numWays == 0)
+      cout << "NONE" << endl;
 
     cin >> t;
     cin >> n;
@@ -34,38 +66,39 @@ int main (void) {
   return 0;
 }
 
-void printSums (int t, int n, const vector<int>& x){
-  cout << "NONE" << endl;
-}
+void search (int partial_sum, vector<int>& selected){
 
-void func (int t, int n, int partial_sum, vector<int>& selected){
-
-  //It only remains to update the maxLeft
-  if (partial_sum == t)
+  if (partial_sum == t) {
     printComponents(selected);
+    numWays++;
+    return;
+  }
 
-  if (partial_sum > t)
+  if (selected.size() == input.size())
+    return;
+
+  if (maxLeft[selected.size()] < (t - partial_sum))
     return;
 
   int position = selected.size();
-  int val = x[position].first;
-  int num = x[position].second;
-  int currentPartail = partial_sum;
+  int val = input [position].first;
+  int num = input [position].second;
+  int currentPartail;
 
-  for (int counter = 1; counter <= num; counter++) {
-    currentPartail += val;
+  for (int counter = num; counter >= 0 ; counter--) {
+    currentPartail = partial_sum + val * counter;
     selected.push_back(counter);
-    func (t, n, currentPartail, selected);
+    search (currentPartail, selected);
     selected.pop_back();
   }
-
 }
 
 
 void printComponents (const vector<int>& selected) {
   int size = selected.size();
-  for (int counter = 0; counter < size; counter++) {
-    for (int elemCounter = 0; elelmCounter < selected[counter]; elemCounter++) {
+  for (int counter = 0; counter < size; counter++) 
+    if (selected[counter]){
+    for (int elemCounter = 0; elemCounter < selected[counter]; elemCounter++) {
       cout << input[counter].first;
       if (elemCounter < selected[counter] - 1)
 	cout << "+";
@@ -73,4 +106,6 @@ void printComponents (const vector<int>& selected) {
     if (counter < size - 1)
       cout << "+";
   }
+  cout << endl;
 }
+
