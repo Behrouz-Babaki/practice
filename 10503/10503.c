f42c1f34419318fa561393e58cbfee3647513f29
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-08-21 21:02:30 +0200

    first implementation. wrong answer on test cases

--- 10503/10503.c
+++ 10503/10503.c
@@ -6,7 +6,12 @@ int n;
 int m;
 int heads[14];
 int tails[14];
+int used [14];
 int fHead, fTail, lHead, lTail;
+int found;
+
+void backtrack(int tail, int position);
+void construct_candidates (int tail, int position, int candidates[], int sides[], int* nCandidates);
 
 int main(void) {
   scanf("%d\n", &n);
@@ -17,9 +22,58 @@ int main(void) {
     int counter;
     for (counter = 0; counter < m; counter++)
       scanf("%d %d\n", &heads[counter], &tails[counter]);
-
-
+    found = 0;
+    backtrack(fTail, -1);
+    if (found)
+      printf("YES\n");
+    else
+      printf("NO\n");
     scanf("%d\n", &n);
   }
   return 0;
+}
+
+void backtrack(int tail, int position) {
+  if (position == n)
+    found = 1;
+  else if (!found) {
+    int nCandidates;
+    int candidates[14];
+    int sides[14];
+    construct_candidates (tail, position + 1, candidates, sides, &nCandidates);
+    int counter;
+    for (counter = 0; !found && counter < nCandidates; counter++) {
+      int nextTail;
+      if (sides[counter])
+	nextTail = tails[candidates[counter]];
+      else
+	nextTail = heads[candidates[counter]];
+      used[candidates[counter]] = 1;
+      backtrack (nextTail, position + 1);
+      used[candidates[counter]] = 0;
+    }
+  }
+}
+
+void construct_candidates (int tail, int position, int candidates[], int sides[], int* nCandidates){
+  *nCandidates = 0;
+  int candId = 0;
+  int counter;
+  for (counter = 0; counter < m; counter++)
+    {
+      if (!used[counter]) {
+	if (heads[counter] == tail &&
+	    (position != (n-1) || tails[counter] == lHead)) {
+	  candidates[candId++] = counter;
+	  sides[candId] = 1;
+	  *nCandidates++;
+	}
+	if (tails[counter] == tail &&
+	    (position != (n-1) || heads[counter] == lHead)) {
+	  candidates[candId++] = counter;
+	  sides[candId] = 0;
+	  *nCandidates++;
+	}
+      }
+    }
 }
