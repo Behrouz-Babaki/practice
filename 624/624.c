89b5445b414859933fb54f59d6791d6d49538cfd
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-08-21 14:47:49 +0200

    624 - first implementation, wrong solution for test cases

---
+++ 624/624.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int tapeLength;
+int durations[20];
+int best, bestCount;
+int bestSol[20];
+int numTracks;
+
+void backtrack(int partial[], int position, int remaining);
+int is_solution (int partial[], int position, int remaining);
+
+int main(void) {
+  int result;
+  /*Arbitrarily chosen constant*/
+  char line[1024];
+  while (fgets(line, sizeof(line), stdin)) {
+    int counter;
+    char *p, *e;
+    long int v;
+    tapeLength = strtol (line, &e, 10);
+    p = e;
+    for (counter = 0; ; p = e,counter++) {
+      v = strtol(p, &e, 10);
+      if (e ==p)
+	break;
+      durations[counter] = v;
+    }
+
+    best = tapeLength;
+    numTracks = counter;
+    int partial [20];
+    backtrack(partial, -1, tapeLength);
+    
+    int sum = 0;
+    for (counter = 0; counter < bestCount; counter++)
+      if(bestSol[counter]){
+	printf("%d ", durations[counter]);
+	sum += durations[counter];
+      }
+    printf("sum:%d\n", sum);
+  }
+  return 0;
+}
+
+void backtrack(int partial[], int position, int remaining) {
+  int waste = is_solution(partial, position, remaining);
+  if (waste >= 0) {
+    if (waste < best) {
+      best = waste;
+      int counter;
+      for (counter = 0; counter < position; counter++)
+	bestSol[counter] = partial[counter];
+      bestCount = position;
+    }
+  }
+
+  else {
+    /* include track */
+    if (durations[position+1] <= remaining) {
+      partial[position+1] = 1;
+      backtrack(partial, position+1, remaining - durations[position+1]);
+    }
+    /* exclude track */
+    partial[position+1] = 0;
+    backtrack(partial, position+1, remaining);
+  }
+}
+
+
+int is_solution (int partial[], int position, int remaining) {
+  int counter;
+  for (counter = position+1; counter < numTracks; counter++)
+    if (durations[counter] < remaining)
+      return -1;
+  return remaining;
+}
+
+
