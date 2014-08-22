e96019e0777fdadff288e0968046994023da887f
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-08-22 14:56:50 +0200

    a different scheme. works on test cases, but gives runtime error on judge

--- 193/193.c
+++ 193/193.c
@@ -1,43 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #pragma GCC diagnostic ignored "-Wunused-result"
-#define VR 1
+#define VR 0
 
 int edges[100][100];
-int black[100];
 int bestBlack[100];
-int colored[100];
 int nNodes;
 int best;
-int nColored;
+int nBlack;
 
-void construct_candidates(int nodeNumber, int candidates[], int* nCandidates);
-void propagate (int nodeNumber, int propagated[], int* nPropagated);
-void undopropagation (int propagated[], int nPropagated);
-void backtrack (int);
+int propagate (int nodeNumber, int color[]);
+void backtrack (int, int[]);
+void process_solution(int[]);
 
 int main(void) {
   int numberOfCases;
   scanf ("%d", &numberOfCases);
+  if (VR)
+    printf ("cases:%d\n", numberOfCases);
   int caseCounter;
   for (caseCounter = 0; caseCounter < numberOfCases; caseCounter++)
     {
       int nEdges;
-      scanf("%d %d\n", &nNodes, &nEdges);
+      scanf("%d %d", &nNodes, &nEdges);
+      if (VR)
+	printf("nodes:%d edges:%d\n", nNodes, nEdges);
       int edgeCounter;
       for (edgeCounter=0; edgeCounter < nEdges; edgeCounter++) {
 	int first, second;
-	scanf("%d %d\n", &first, &second);
+	scanf("%d %d", &first, &second);
 	edges[first-1][second-1] = 1;
 	edges[second-1][first-1] = 1;
       }
+
+      int color[100];
       int counter;
       for (counter=0; counter < nNodes; counter++) 
-	black[counter] = 0;
+	color[counter] = -1;
 
       best = 0;
-      nColored = 0;
-      backtrack(0);
+      nBlack = 0;
+      backtrack(0, color);
 
       printf("%d\n%d", best, bestBlack[0]+1);
       for (counter=1; counter < best; counter++)
@@ -48,63 +51,88 @@ int main(void) {
   return 0;
 }
 
-void backtrack(int nodeNumber) {
+void backtrack(int nodeNumber, int prevColor[]) {
   int counter;
+  int prevNBlack = nBlack;
+  int color[nNodes];
+  for (counter = 0; counter < nNodes; counter++)
+    color[counter] = prevColor[counter];
 
-  if (nodeNumber == nNodes) {
-    if (nColored > best) {
-      best = nColored;
-      int id = 0;
-      for (counter=0; counter < nNodes; counter++)
-	if (black[counter])
-	  bestBlack[id++] = counter;
-    }
-    return;
+  int nextNode;  
+  color[nodeNumber] = 1;
+  nBlack++;
+
+  int found = 0;
+  if (propagate (nodeNumber, color)) {
+    for (counter = nodeNumber + 1; !found && counter < nNodes; counter++)
+      if (color[counter] < 0) {
+	nextNode = counter;
+	found = 1;
+      }
+    if (found)
+      backtrack(nextNode, color);
+    else 
+      process_solution(color);
   }
 
-  if (colored[nodeNumber])
-    backtrack(nodeNumber+1);
+  nBlack = prevNBlack;
+  for (counter = 0; counter < nNodes; counter++)
+    color[counter] = prevColor[counter];
+  color[nodeNumber] = 0;
 
+  if (propagate (nodeNumber, color)) {
+    found = 0;
+    for (counter = nodeNumber + 1; !found && counter < nNodes; counter++)
+      if (color[counter] < 0) {
+	nextNode = counter;
+	found = 1;
+      }
+    if (found)
+      backtrack(nextNode, color);
+    else 
+      process_solution(color);
+  }
+}
 
-  colored[nodeNumber] = 1;
-  black[nodeNumber] = 1;
-  nColored++;
-  /*1 is black*/
-  int propagated[nNodes];
+int propagate (int nodeNumber, int color[]) {
+  int success = 1;
+  int propagated[nodeNumber];
   int nPropagated = 0;
-  propagate (nodeNumber, propagated, &nPropagated);
-  backtrack(nodeNumber+1);
-  undopropagation (propagated, nPropagated);
-  nColored--;
-
-  black[nodeNumber] = 0;
-  propagate (nodeNumber, propagated, &nPropagated);
-  backtrack(nodeNumber+1);
-  undopropagation (propagated, nPropagated);
-  colored[nodeNumber] = 0;
-
-}
-
-
-void propagate (int nodeNumber, int propagated[], int* nPropagated) {
+  if (VR)
+    printf("propagating for %d\n", nodeNumber);
   int counter;
-  for (counter=0; counter < nNodes; counter++)
-    if (edges[nodeNumber][counter] && !colored[counter]) {
-      black[counter] = 1-black[nodeNumber];
-      if (black[counter])
-	nColored++;
-      colored[counter] = 1;
-      propagated[*nPropagated] = counter;
-      (*nPropagated)++;
-      propagate (counter, propagated, nPropagated);
+  for (counter=0; success && counter < nNodes; counter++)
+    if (edges[nodeNumber][counter]) {
+      if (color[counter] >= 0) {
+	if (color[nodeNumber] == 1 && color[counter] == 1)
+	  success = 0;
+      }
+      else {
+	color[counter] = 1-color[nodeNumber];
+	propagated[nPropagated++] = counter;
+	if (VR)
+	  printf("setting %d to %d\n", counter, color[counter]);
+	if (color[counter])
+	  nBlack++;
+      }
     }
+
+  for (counter = 0; success && counter < nPropagated; counter++)
+    success = propagate(propagated[counter], color);
+
+  return success;
 }
 
-void undopropagation (int propagated[], int nPropagated) {
-  int counter;
-  for (counter = 0; counter < nPropagated; counter++) {
-    if (black[propagated[counter]])
-      nColored--;
-    colored[propagated[counter]] = 0;
+
+void process_solution(int color[]) {
+  if (nBlack > best) {
+    best = nBlack;
+    if (VR)
+      printf("best is now %d\n", best);
+    int id = 0;
+    int counter;
+    for (counter=0; counter < nNodes; counter++)
+      if (color[counter] == 1)
+	bestBlack[id++] = counter;
   }
 }
