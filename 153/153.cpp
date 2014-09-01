26b39536ab9f15c6f574100005f03b1a2aad541e
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-09-01 16:16:26 +0200

    a first (non-working) implementation of 153

---
+++ 153/153.cpp
@@ -0,0 +1,69 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <map>
+
+using std::map;
+
+size_t countPre(char*, size_t, size_t);
+size_t countPermutations (const map<char,size_t>&, char, char);
+size_t factorial(size_t);
+
+int main(void) {
+  char str[31];
+
+  do {
+    scanf("%s", str);
+    size_t count;
+    count = countPre(str, 0, strlen(str));
+    printf("%10zd\n", count);
+  } while (str[0] != '#');
+
+  return 0;
+}
+
+size_t countPre (char* str, size_t start, size_t end) {
+  
+  if (start == end-1)
+    return 1;
+  
+  size_t count = 0;
+  map <char, size_t> letterCounts;
+  char first = str[start];
+  for (size_t counter = start + 1; counter < end; counter++)
+    {
+      char letter = str[counter];
+      if (letterCounts.find(str[counter]) == letterCounts.end())
+	letterCounts[letter] = 1;
+      else 
+	// supicious
+	letterCounts[letter] ++;
+    }
+  for (map<char,size_t>::iterator itr = letterCounts.begin(), endItr = letterCounts.end(); 
+       (itr->first) < first && itr != endItr; itr++)
+    count += countPermutations (letterCounts, first, itr->first);
+
+  count += countPre (str, start+1, end);
+  return count;
+}
+
+size_t countPermutations (const map<char,size_t>& letterCounts, char first, char cand) {
+  size_t totalCount = 0;
+  for (map<char,size_t>::const_iterator itr = letterCounts.begin(), endItr = letterCounts.end();
+       itr != endItr; itr++)
+    totalCount += itr->second;
+  totalCount --;
+  size_t perms = factorial(totalCount);
+  for (map<char,size_t>::const_iterator itr = letterCounts.begin(), endItr = letterCounts.end();
+       itr != endItr; itr++)
+    if (itr->first != cand)
+      perms /= itr->second;
+    else if (itr->second > 1)
+      perms /= ((itr->second) - 1);
+
+  return perms;
+}
+
+size_t factorial (size_t x) {
+  return (x == 1 ? x : x*factorial(x-1));
+}
