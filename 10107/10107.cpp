3850da38f1955a3905b0f2fdc3dc976b132b9b23
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-10-08 19:02:35 +0200

    starting with 10107

---
+++ 10107/10107.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+#include <functional>
+
+using std::endl;
+using std::cout;
+using std::cin;
+using std::priority_queue;
+using std::less;
+using std::greater;
+
+int main(void) {
+  int val;
+  while (cin >> val) {
+  }
+  return 0;
+}
