f756b6ab5bb3ea9b7cc76fac3a6b5395f21296ad
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-10-12 00:32:45 +0200

    starting 732

---
+++ 732/732.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(void) {
+  string line1;
+  string line2;
+  while (getline(cin, line1)) {
+    getline(cin, line2);
+
+
+  }
+  return 0;
+}
