f279df7b87798237ee62180832208b3377c68f4a
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-11-20 19:36:16 +0100

    still producing wrong answer

--- 657/657.cpp
+++ 657/657.cpp
@@ -26,10 +26,13 @@ typedef vector<vs> vvs;
 #define TRvss(c, it) \
   for (vss::iterator it = (c).begin(), end = (c).end(); it != end; it++)
 
-size_t dfs_dice(vvs& types, vvs& visited, size_t row, size_t col);
+void dfs_dice(vvs& types, vvs& visited, size_t row, size_t col);
 void dfs_dots(vvs& types, vvs& visited, size_t row, size_t col);
 vss neighbours (size_t row, size_t col, size_t w, size_t h);
 void dfs_background(vvs& types, vvs& visited, size_t row, size_t col);
+
+vs num_dots;
+size_t dots;
 
 int main(void) {
   size_t w, h;
@@ -55,64 +58,84 @@ int main(void) {
 	}
     }
     
-    vs num_dots;
+    num_dots.clear();
+    dots = 0;
     for (size_t row_counter = 0; row_counter < h; row_counter++) 
       for (size_t col_counter = 0; col_counter < w; col_counter++) 
-	if (!visited[row_counter][col_counter]){ 
-	  if(types[row_counter][col_counter] == 2){
-	    //cout << "\nnext@" << row_counter << ":" << col_counter << endl;
-	    num_dots.push_back(dfs_dice(types, visited, row_counter, col_counter));
-	  }
-	  else
+	if (!visited[row_counter][col_counter])
+	  switch(types[row_counter][col_counter]){ 
+	  case 1:
 	    dfs_background(types, visited, row_counter, col_counter);
+	    break;
+	  case 2:
+	    dots = 0;
+	    dfs_dice(types, visited, row_counter, col_counter);
+	    num_dots.push_back(dots);
+	    break;
+	  case 3:
+	    dots = 1;
+	    dfs_dots(types, visited, row_counter, col_counter);
+	    num_dots.push_back(dots);
+	    break;
 	}
 	  
     sort(num_dots.begin(), num_dots.end());
     cout << "Throw " << case_counter++ << endl;
-    for (size_t dot_cnt = 0, size = num_dots.size(); dot_cnt < size-1; dot_cnt++)
-      cout << num_dots[dot_cnt] << " ";
-    cout << num_dots[num_dots.size()-1] << endl;
-    cout << endl;
+    for (size_t dot_cnt = 0, size = num_dots.size(); dot_cnt < size; dot_cnt++) {
+      cout << num_dots[dot_cnt];
+      if (dot_cnt < size-1)
+	cout << " ";
+    }
+    cout << endl << endl;
     cin >> w >> h >> ws;
   }
   return 0;
 }
 
-size_t dfs_dice(vvs& types, vvs& visited, size_t row, size_t col) {
+void dfs_dice(vvs& types, vvs& visited, size_t row, size_t col) {
   //cout << "D" << row << ":" << col << "\t";
-  size_t dots = 0;
+  visited[row][col] = 1;
   vss n = neighbours(row, col, types[0].size(), types.size());
   TRvss (n, it) 
     if (!visited[it->first][it->second]) {
-      visited[it->first][it->second] = 1;
       if (types[it->first][it->second] == 2)
-	dots += dfs_dice(types, visited, it->first, it->second);
-      else if (types[it->first][it->second] == 3){
-	dots++;
+	dfs_dice(types, visited, it->first, it->second);
+      else if (types[it->first][it->second] == 3) {
 	dfs_dots(types, visited, it->first, it->second);
+	dots++;
       }
   }
-  return dots;
 }
 
 void dfs_dots(vvs& types, vvs& visited, size_t row, size_t col) {
   //cout << "T" << row << ":" << col << "\t";
+  visited[row][col] = 1;
   vss n = neighbours(row, col, types[0].size(), types.size());
+  bool done = true;
   TRvss (n, it) 
-    if (!visited[it->first][it->second] && types[it->first][it->second] == 3) {
-      visited[it->first][it->second] = 1;
-      dfs_dots(types, visited, it->first, it->second);
-    }
+    if (!visited[it->first][it->second] && types[it->first][it->second] == 3) 
+      done = false;
+
+  if (done) {
+    //cout <<"done@" << row <<":" << col << endl;
+    TRvss (n, it) 
+      if (!visited[it->first][it->second] && types[it->first][it->second] == 2) 
+	dfs_dice(types, visited, it->first, it->second);
+  }
+  else
+    TRvss (n, it) 
+      if (!visited[it->first][it->second] && types[it->first][it->second] == 3) 
+	dfs_dots(types, visited, it->first, it->second);
+      
 }
 
 void dfs_background(vvs& types, vvs& visited, size_t row, size_t col) {
   //cout << "B" << row << ":" << col << "\t";
+  visited[row][col] = 1;
   vss n = neighbours(row, col, types[0].size(), types.size());
   TRvss (n, it) 
-    if (!visited[it->first][it->second] && types[it->first][it->second] == 1) {
-      visited[it->first][it->second] = 1;
+    if (!visited[it->first][it->second] && types[it->first][it->second] == 1) 
       dfs_background(types, visited, it->first, it->second);
-    }
 }
 
 vss neighbours (size_t row, size_t col, size_t w, size_t h) {
