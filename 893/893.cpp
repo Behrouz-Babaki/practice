0fd48f611f6321b4babee7e4a87b5ae81cba5e81
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-03-29 00:06:46 +0100

    893 accepted

--- 893/893.cpp
+++ 893/893.cpp
@@ -4,12 +4,59 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+bool is_leap(int);
+int n_days[2][12] = {{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+		     {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+
 int main(void) {
   int n, day, month, year;
   cin >> n >> day >> month >> year;
   while (n || day || month || year) {
-    cout << day << " " << month << " " << year << endl;
+    int leap = is_leap(year);
+    int current_month = month;
+    if ((n+day) <= n_days[leap][month-1]) {
+      cout << day+n << " " << month << " " << year << endl;
+      cin >> n >> day >> month >> year;
+      continue;
+    }
+    else {
+      n -= (n_days[leap][month-1] - day);
+      current_month++;
+    }
+    while (current_month <= 12 && n > n_days[leap][current_month-1]) {
+      n -= (n_days[leap][current_month-1]);
+      current_month ++;
+    }
+    if (current_month <= 12 && n <= n_days[leap][current_month-1]) {
+      cout << n << " " << current_month << " " << year << endl;
+      cin >> n >> day >> month >> year;
+      continue;
+    }
+    year++;
+    int year_days;
+    year_days = is_leap(year) ? 366 : 365;
+    while (n > year_days) {
+      n -= year_days;
+      year++;
+      year_days = is_leap(year) ? 366 : 365;
+    }
+    current_month = 1;
+    leap = is_leap(year);
+    while (n > n_days[leap][current_month-1]) {
+      n -= (n_days[leap][current_month-1]);
+      current_month++;
+    }
+    cout << n << " " << current_month << " " << year << endl;
     cin >> n >> day >> month >> year;
   }
   return 0;
+}
+
+bool is_leap(int year) {
+  if (year%4 != 0)
+    return false;
+
+  if (year%100 != 0 || year%400 == 0)
+    return true;
+  return false;
 }
