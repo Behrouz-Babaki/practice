6a691fbc981c80a33d865285514552963854befd
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-03-28 18:42:22 +0100

    starting 893

--- 893/893.cpp
+++ 893/893.cpp
@@ -2,7 +2,14 @@
 
 using std::cout;
 using std::endl;
+using std::cin;
 
 int main(void) {
-  cout << "Hello World!" << endl;
+  int n, day, month, year;
+  cin >> n >> day >> month >> year;
+  while (n || day || month || year) {
+    cout << day << " " << month << " " << year << endl;
+    cin >> n >> day >> month >> year;
+  }
+  return 0;
 }
