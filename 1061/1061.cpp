64b43f02d83395010f7f80c0340b1ee30694ddf5
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-03-29 01:21:21 +0100

    starting 1061

--- 1061/1061.cpp
+++ 1061/1061.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using std::cout;
 using std::endl;
+using std::string;
 
 int main(void) {
-  cout << "Hello World!" << endl;
+  string p1, p2, child;
+  cin >> p1 >> p2 >> child;
+  while (p1.compare("E") || p2.compare("N") || child.compare("D")) {
+    
+    cin >> p1 >> p2 >> child;
+  }
+  return 0;
 }
