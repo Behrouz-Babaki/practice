b0e44d31f11be771ef491ac3b327bf699519db1c
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-03-31 03:16:24 +0200

    starting 11034

--- 11034/11034.cpp
+++ 11034/11034.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <string>
+#include <queue>
 
 using std::cout;
+using std::cin;
 using std::endl;
+using std::string;
+using std::queue;
 
 int main(void) {
-  cout << "Hello World!" << endl;
+  int cnt;
+  cin >> cnt;
+  while (cnt--) {
+    int l, m;
+    cin >> l >> m;
+    queue<int> left, right;
+    while (m--) {
+      int length;
+      string side;
+      cin >> length >> side;
+      if (!side.compare("left"))
+	left.push(length);
+      else
+	right.push(length);
+    }
+
+  }
+  return 0;
 }
