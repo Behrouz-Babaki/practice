8131f9ec6bab58b045b7ff5f0a7e4d40089b9782
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-03-31 03:45:38 +0200

    still not done

--- 11034/11034.cpp
+++ 11034/11034.cpp
@@ -24,7 +24,25 @@ int main(void) {
       else
 	right.push(length);
     }
+    int side = 0;
+    int times = 0;
+    while (!left.empty() || !right.empty()) {
+      int rem = l * 100;
+      queue<int>& q = side ? right : left;
+      queue<int>& other = side ? left : right;
 
+      while (!q.empty() && rem > q.front()) {
+	rem -= q.front();
+	q.pop();
+      }
+      if (!other.empty()) {
+	side = !side;
+	times++;
+      }
+      else if (!q.empty()) 
+	times += 2;
+    }
+    cout << times << endl;
   }
-  return 0;
-}
+    return 0;
+  }
