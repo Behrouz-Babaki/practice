2c74f896779c13d53fc572a7e2243fade53a53b9
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-04-04 21:51:50 +0200

    starting 1062 and 1203

--- 1062/1062.cpp
+++ 1062/1062.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <string>
+#include <map>
+#include <vector>
 
 using std::cout;
+using std::cin;
 using std::endl;
+using std::string;
+using std::map;
+using std::vector;
 
 int main(void) {
-  cout << "Hello World!" << endl;
+  string line;
+  cin >> line;
+  int case_num = 1;
+  while (line != "end") {
+    map<char, vector<int> > containers;
+    for (int cnt = 0, size = line.size(); cnt < size; cnt++) {
+      char ship = line[cnt];
+      if (containers.find(ship) == containers.end()) 
+	containers[ship] = vector<int> ();
+      containers[ship].push_back(cnt);
+    }
+    int min_stack = line.length();
+    //perform the search here!
+    cout << "Case " << case_num++ << ": " << min_stack << endl;
+    // for (auto c : containers) {
+    //   cout << c.first << " : ";
+    //   for (auto n : c.second)
+    // 	cout << n << " ";
+    //   cout << endl;
+    // }
+    // cout << endl;
+    cin >> line;
+  }
+  return 0;
 }
--- 1203/1203.cpp
+++ 1203/1203.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <string>
+#include <utility>
+#include <set>
+#include <queue>
+#include <vector>
+#include <functional>
 
 using std::cout;
+using std::cin;
 using std::endl;
+using std::string;
+using std::pair;
+using std::make_pair;
+using std::set;
+using std::priority_queue;
+using std::vector;
+using std::greater;
 
 int main(void) {
-  cout << "Hello World!" << endl;
+  string kw;
+  int id, period;
+  set<pair<int, int> >queries;
+  cin >> kw;
+  while (kw != "#") {
+    cin >> id >> period;
+    queries.insert(make_pair(period, id));
+    cin >> kw;
+  }
+  int k; 
+  cin >> k;
+  //priority_queue<pair<int,int> , vector<pair<int,int> >, greater<pair<int, int> > > top;
+  priority_queue<pair<int,int> > results;
+  int num = (k / queries.size()) + 1;
+  for (auto q : queries) 
+    for (int cnt = 1; cnt <= num; cnt++) {
+      results.push(make_pair(-q.first*cnt, -q.second));
+    }
+  for (int cnt= 0; cnt < k; cnt++){
+    cout << -results.top().second << endl;
+    results.pop();
+  }
+  return 0;
 }
