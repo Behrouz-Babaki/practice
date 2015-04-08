2e52ed04eac49b531a57aabdaaa04182fd4d822a
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-04-08 15:48:35 +0200

    11553 accepted

--- 11553/11553.cpp
+++ 11553/11553.cpp
@@ -19,36 +19,26 @@ int main(void) {
     for (int i = 0; i < size; i++)
       for (int j = 0; j < size; j++)
 	cin >> grid[i][j];
-    vector<int> alice(size);
+
     vector<int> bob(size);
-    for (int i = 0; i < size; i++) {
-      alice[i] = i;
+    for (int i = 0; i < size; i++) 
       bob[i] = i;
-    }
 
     int best_gain;
-    bool first_alice = true;
-    do {
-      bool first_bob = true;
-      int best_bob;
-      do {
-	int current_gain = 0;
-	for (int i = 0; i < size; i++) 
-	  current_gain += grid[alice[i]][bob[i]];
-	  if (first_bob) {
-	    first_bob = false;
-	    best_bob = current_gain;
-	  }
-	  else if (current_gain < best_bob) 
-	    best_bob = current_gain;
-      } while (next_permutation(bob.begin(), bob.end()));
-      if (first_alice) {
-	first_alice = false;
-	best_gain = best_bob;
+    bool first = true;
+    do{
+      int current_gain = 0;
+      for (int i = 0; i < size; i++)
+	current_gain += grid[i][bob[i]];
+      if (first) {
+	best_gain = current_gain;
+	first = false;
       }
-      else if (best_bob > best_gain)
-	best_gain = best_bob;
-      } while (next_permutation(alice.begin(), alice.end()));
+      if (best_gain > current_gain)
+	best_gain = current_gain;
+
+    } while (next_permutation(bob.begin(), bob.end()));
+
       cout << best_gain << endl;
     }
     return 0;
