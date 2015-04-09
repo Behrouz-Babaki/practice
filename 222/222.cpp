9584a287a8b7c1989a397c0caab1890c5ea15b93
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-04-09 12:27:48 +0200

    222 accepted

--- 222/222.cpp
+++ 222/222.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <cmath>
 
 using std::cin;
 using std::cout;
@@ -18,9 +19,10 @@ vector<double> distances;
 vector<double> prices;
 int num_station;
 double mpg;
+vector<int> v;
 
 double round_cents(double price);
-void search(int id, bool choice, double cost, double gallons, double dist);
+void search(int id, double cost, double gallons, double dist);
 
 int main(void) {
 
@@ -29,9 +31,7 @@ int main(void) {
   while (total_distance >= 0) {
     double o_cost;
     cin >> capacity >> mpg >> o_cost;
-    double cost = round_cents(capacity*o_cost);
-    //check if 2-dollar-rule also applies to the first station
-    cost += 2;
+    double cost = round_cents(o_cost);
     cin >> num_station;
     distances.clear();
     distances.resize(num_station);
@@ -43,51 +43,57 @@ int main(void) {
     }
     // search (id, choice, cost)
     first = true;
-    search(-1, 0, cost, capacity, 0);
+    v.clear();
+    search(0, cost, capacity, 0);
     cout << "Data Set #" << case_number++ << endl;
-    cout << "minimum cost = $" << fixed << setprecision(2) << min_cost/100 << endl;
+    cout << "minimum cost = $" << fixed << setprecision(2) << min_cost << endl;
     cin >> total_distance;
   }
   return 0;
 }
 
-// We are at station id, have made choice for this station. We had gallons of gas at distance dist.
-void search(int id, bool choice, double cost, double gallons, double dist) {
-  if (id+1 == num_station) {
-    if (cost < min_cost || first)
+// We are at station id. We had gallons of gas at distance dist.
+void search(int id, double cost, double gallons, double dist) {
+  if (id == num_station) {
+    if (cost < min_cost || first) {
       min_cost = cost;
+      // for (auto vv : v)
+      // 	cout << vv << "-";
+      // cout << "> " << min_cost << endl;
+    }
     if (first)
       first = false;
     return;
   }
-  gallons -= (distances[id] - dist) / mpg;
-  if (choice) {
-    cost += round_cents(prices[id] * (capacity - gallons));
-    cost += 2;
-    gallons = capacity;
-    dist = distances[id];
-  }
-  
+
   double later_dist;
-  if (id+1 < num_station-1) 
-    later_dist = distances[id+2];
+  if (id+1 < num_station) 
+    later_dist = distances[id+1];
   else 
     later_dist = total_distance;
+
   bool stop = false, pass = false;
   if ((later_dist - dist)/mpg > gallons)
     stop = true;
-  if ((distances[id+1] - dist)/mpg > capacity/2)
+  if (!stop && (distances[id] - dist)/mpg < capacity/2)
     pass = true;
 
-  if (!pass) 
-    search(id+1, 1, cost, gallons, dist);
+  if (!pass) {
+    double new_cost = cost;
+    double remaining = gallons - (distances[id] - dist)/mpg;
+    new_cost += round_cents(prices[id] * (capacity - remaining)/100);
+    new_cost += 2;
+    v.push_back(id);
+    search(id+1, new_cost, capacity, distances[id]);
+    v.pop_back();
+  }
 
   if (!stop)
-    search(id+1, 0, cost, gallons, dist);
+    search(id+1, cost, gallons, dist);
     
 }
 
 
 double round_cents(double price) {
-  return price;
+  return round(price*100)/100;
 }
