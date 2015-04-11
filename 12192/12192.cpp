179d040d66db79976626abccac31c1554bea9c57
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-04-11 23:33:48 +0200

    12192 accepted

--- 12192/12192.cpp
+++ 12192/12192.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
+using std::lower_bound;
+using std::upper_bound;
 
 int main(void) {
   int rows, cols;
@@ -39,7 +42,22 @@ int main(void) {
     while (q--) {
       int l, u;
       cin >> l >> u;
-      // answer here
+      int best;
+      bool first = true;
+      for (int i=0; i<rows+cols-1; i++) {
+	vector<int>::iterator lo = 
+	  lower_bound(diags[i].begin(),diags[i].end(), l);
+	vector<int>::iterator hi = 
+	  upper_bound(diags[i].begin(),diags[i].end(), u);
+	int diff = hi-lo;
+	if (first) {
+	  best=diff;
+	  first = false;
+	}
+	else if (diff>best)
+	  best=diff;
+      }
+      cout << best << endl;
     }
     cout << '-' << endl;
     cin >> rows >> cols;
