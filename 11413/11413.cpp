2cba143efa44313b8868d804301f6e2af1d686c0
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-04-12 02:02:45 +0200

    11413 accepted

--- 11413/11413.cpp
+++ 11413/11413.cpp
@@ -14,22 +14,38 @@ int main(void) {
   while (cin >> num_v >> num_c) {
     vessels.clear();
     vessels.resize(num_v);
-    for (int i=0; i<num_v; i++)
+    int sum=0;
+    for (int i=0; i<num_v; i++){
       cin >> vessels[i];
-
+      sum+=vessels[i];
+    }
+    int hi=sum, lo=0, mid;
+    while (hi>lo+1){
+      mid=(hi+lo)/2;
+      if(can(mid, num_c))
+	hi=mid;
+      else
+	lo=mid;
+    }
+    cout << hi << endl;
   }
 }
 
 bool can(int capacity, int num_c){
-  bool finish=false;
+  bool fail=false;
   int sum = 0, cnt=1;
   for(vector<int>::iterator itr=vessels.begin(),end=vessels.end();
-      cnt <= num_c && itr!=end; itr++)
-    if(sum+*itr>capacity) {
+      !fail && itr!=end; itr++)
+    if (*itr>capacity)
+      fail=true;
+    else if(sum+*itr>capacity) {
       cnt++;
+      if (cnt>num_c)
+	fail=true;
       sum=*itr;
     }
     else
       sum += *itr;
-  return (cnt <= num_c);
+  
+  return !fail;
 }
