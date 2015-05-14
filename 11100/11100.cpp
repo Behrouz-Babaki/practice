70668265db14d1dc7500799e4bd0ba5aa66fb6a3
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-05-15 00:13:08 +0200

    11100 accepted

--- 11100/11100.cpp
+++ 11100/11100.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
 using std::sort;
-using std::set;
 
 int main(void){
   int num_bags;
@@ -18,30 +16,51 @@ int main(void){
     for (int cnt=0; cnt<num_bags; cnt++)
       cin >> bags[cnt];
     sort(bags.begin(), bags.end());
-
-    vector<vector<int> > collections;
-    while(bags.size() > 0){
-      vector<int> collected;
-      set<int> removed;
-      for (int i=0; i<bags.size(); i++)
-	if (collected.empty() || 
-	    bags[i] > collected.back()){
-	  collected.push_back(bags[i]);
-	  removed.insert(i);
-	}
-
-      collections.push_back(collected);
-
-      vector<int> bags_copy;
-      for (int i=0; i<bags.size(); i++)
-	if (removed.find(i)==removed.end())
-	  bags_copy.push_back(bags[i]);
-      bags = bags_copy;
+    int id = 0, max = 0, count = 0;
+    while (id < bags.size()) {
+      count = 0;
+      bool done = false;
+      while(!done){
+	count++;
+	id++;
+	if (id >= bags.size() ||
+	    bags[id]!=bags[id-1])
+	  done = true;
+      }
+      if (count > max)
+	max = count;
     }
+
+    vector<vector<int> > collections(max);
+    id=0;
+    while (id < bags.size()){
+      int col_cnt = 0;
+      bool done = false;
+      vector<bool> used(max, false);
+      while(!done){
+	int min_ind = 0;
+	for (int cnt=0; cnt<max; cnt++)
+	  if (!used[cnt] &&
+	      collections[cnt].size() < collections[min_ind].size()){
+	    min_ind = cnt;
+	  }
+	collections[min_ind].push_back(bags[id]);
+	used[min_ind] = true;
+	col_cnt++;
+	id++;
+	if (id >= bags.size() || bags[id]!=bags[id-1])
+	  done = true;
+      }
+    }
+
     cout << collections.size() << endl;
     for (auto collection : collections){
-      for (auto bag : collection)
-	cout << bag << " ";
+      int size = collection.size();
+      for (int cnt=0; cnt<size; cnt++) {
+	if (cnt)
+	  cout << " ";
+	cout << collection[cnt];
+      }
       cout << endl;
     }
     cin >> num_bags;
