6248462d816bed48ebbe90a1d867c2e598941a23
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-05-15 01:35:33 +0200

    10656 accepted

--- 10656/10656.cpp
+++ 10656/10656.cpp
@@ -13,25 +13,17 @@ int main(void) {
     vector<int> seq(num);
     for (int cnt=0; cnt<num; cnt++)
       cin >> seq[cnt];
-    int b = -1, e = -1, i=0;
-    bool inzero=false;
-    while (i<num){
-      if (seq[i]==0 && !inzero)
-	e = i;
-      inzero = (seq[i]==0);
-      if (b==-1 && seq[i]>0)
-	b = i;
-      i++;
-    }
-    if (e == -1)
-      e = seq.size();
-    if (b == -1)
+    vector<int> nonz;
+    bool any=false;
+    for (int cnt=0; cnt<num; cnt++)
+      if (seq[cnt]) {
+	if (any)
+	  cout << " ";
+	cout << seq[cnt];
+	any = true;
+      }
+    if (!any)
       cout << 0;
-    for (int i=b; i<e; i++) {
-      if (i>b)
-	cout << " ";
-      cout << seq[i];
-    }
     cout << endl;
     cin >> num;
   }
