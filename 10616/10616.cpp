d775be93af0d3d516e1bd792c0b72c379f803b66
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-08-03 16:48:34 +0200

    10616 still TLE

--- 10616/10616.cpp
+++ 10616/10616.cpp
@@ -10,23 +10,26 @@ using std::vector;
 using std::sort;
 using std::map;
 
-int get_sum(int id, int M, int sum);
+int get_sum(int id, int M, int D, int sum);
 class triplet {
 public:
-  triplet(int a, int b, int c) 
-    : _a(a) , _b(b), _c(c) {}
+  triplet(int a, int b, int c, int d) 
+    : _a(a) , _b(b), _c(c), _d(d) {}
   bool operator<(const triplet& rhs) const {
     if (rhs._a != _a)
       return (rhs._a < _a);
     if (rhs._b != _b)
       return (rhs._b < _b);
-    return (rhs._c < _c);
+    if (rhs._c != _c)
+      return (rhs._c < _c);
+    return (rhs._d < _d);
   }
-  int _a, _b, _c;
+  int _a, _b, _c, _d;
 };
 
 map<triplet, int> total_map;
 vector<int> numbers;
+vector<int> totals;
 int N, Q;
 
 int main(void) {
@@ -36,7 +39,8 @@ int main(void) {
     total_map.clear();
     numbers.clear();
     numbers.resize(N);
-    vector<int> totals(N);
+    totals.clear();
+    totals.resize(N);
     for (int num_cnt = 0; num_cnt < N; num_cnt++)
       cin >> numbers[num_cnt];
     sort(numbers.rbegin(), numbers.rend());
@@ -49,30 +53,33 @@ int main(void) {
     int D, M;
     for (int q_cnt = 0; q_cnt < Q; q_cnt++) {
       cin >> D >> M;
-      int sol = 0;
-      int mult = 1;
-      while (D*mult <= totals[M-1]) {
-	sol += get_sum(0, M, D*mult);
-	mult++;
-      }
-      cout << "QUERY " << q_cnt+1 << ": " << sol << endl;
+      cout << "QUERY " << q_cnt+1 << ": " << get_sum(0, M, D, 0) << endl;
     }
     cin >> N >> Q;
   }
   return 0;
 }
 
-int get_sum(int id, int M, int sum) {
-  if (id == N || M == 0 || sum <= 0)
+int get_sum(int id, int M, int D, int res) {
+  if (M > (N - id))
     return 0;
-  if (M == 1 && sum == numbers[id])
+  if (M == 0 && res == 0)
     return 1;
-  triplet t(id, M, sum);
+  if (id == N)
+    return 0;
+  // if (M == 1 && res == (numbers[id]%D))
+  //   return 1;
+  triplet t(id, M, D, res);
   if (total_map.find(t) != total_map.end())
     return total_map[t];
   int count;
-  count = get_sum(id+1, M, sum) + 
-    get_sum(id+1, M-1, sum-numbers[id]);
+  int res2;
+  if (res >= (numbers[id]%D))
+    res2 = res - (numbers[id]%D);
+  else
+    res2 = res - (numbers[id]%D) + D;
+  count = get_sum(id+1, M, D, res) + 
+    get_sum(id+1, M-1, D, res2);
   total_map[t] = count;
   return count;
 }
