20819c758d384274ca408bda4a6c008805c60d59
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-08-04 15:00:36 +0200

    481 accepted -- code copied from pseudocode in wikipedia

--- 481/481.cpp
+++ 481/481.cpp
@@ -12,36 +12,37 @@ int main(void) {
   while (cin >> val)
     seq.push_back(val);
   int n = seq.size();
-  vector<int> sz(n);
-  vector<int> next(n);
-  sz[n-1] = 1;
-  next[n-1] = -1;
-  for (int i = n-2; i >= 0; i--) {
-    int max_val = 0;
-    int id = -1;
-    for (int j = n-1; j > i; j--) 
-      if (seq[j] > seq[i] && sz[j] > max_val) {
-	max_val = sz[j];
-	id = j;
-      }
-    sz[i] = max_val + 1;
-    next[i] = id;
-  }
+  vector<int> M(n+1), P(n, -1);
+  int L = 0;
   
-  int max_id = n-1;
-  int max_val = 1;
-  for (int i = n-2; i >=0; i--)
-    if (sz[i] > max_val) {
-      max_id = i;
-      max_val = sz[i];
+  for (int i=0; i<n; i++) {
+    int k;
+    //binary search k in seq[M[1]]..seq[M[L]]
+    int lo = 1, hi = L, mid;
+    while (lo <= hi) {
+      mid = (lo + hi)/2;
+      if (seq[M[mid]] < seq[i])
+	lo = mid + 1;
+      else 
+	hi = mid - 1;
     }
-  
-  cout << max_val << endl;
-  cout << "-" << endl;
-  int id = max_id;
-  while (id >= 0) {
-    cout << seq[id] << endl;;
-    id = next[id];
+
+    int newL = lo;
+    P[i] = M[newL-1];
+    M[newL] = i;
+
+    if (newL > L)
+      L = newL;
   }
+
+  vector<int> res(L);
+  int k = M[L];
+  for (int i = L-1; i>=0; i--){
+    res[i] = seq[k];
+    k = P[k];
+  }
+  cout << L << "\n" << "-" << endl;
+  for (auto i : res)
+    cout << i << endl;
   return 0;
 }
