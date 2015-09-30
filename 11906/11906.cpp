5657906ee053b4d5cc04263583d7eae718a8cebe
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-10-01 01:11:18 +0200

    11906 WA

--- 11906/11906.cpp
+++ 11906/11906.cpp
@@ -9,6 +9,7 @@ using std::vector;
 vector<vector<int> > visited;
 vector<vector<int> > accessible;
 int R, C, M, N;
+int odds, evens;
 
 void dfs(int row, int col);
 void print_vec(vector<vector<int> >& v);
@@ -32,27 +33,8 @@ int main(void) {
       cin >> wr >> wc;
       accessible[wr][wc] = 0;
     }
-
+    odds = evens = 0;
     dfs(0,0);
-    // print_vec(visited);
-    int odds = 0, evens = 0;
-    for (int r=0; r<R; r++)
-      for (int c=0; c<C; c++) 
-	if (visited[r][c]){
-	int cnt = 0; 
-	for (int rr=-1; rr<=1; rr+=2)
-	  for(int cc=-1; cc<=1; cc+=2) {
-	    int row = r+rr*N;
-	    int col = c+cc*M;
-	    if (row >=0 && row < R &&
-		col >=0 && col < C && visited[row][col])
-	      cnt++;
-	  }
-	if (cnt%2)
-	  odds++;
-	else
-	  evens++;
-      }
     cout << "Case " << case_cnt << ": " << evens << " " << odds << endl;
     case_cnt++;
   }
@@ -60,6 +42,7 @@ int main(void) {
 }
 
 void dfs(int r, int c) {
+  int cnt = 0;
   visited[r][c] = 1;
   for (int rr=-1; rr<=1; rr+=2)
     for(int cc=-1; cc<=1; cc+=2) {
@@ -67,10 +50,28 @@ void dfs(int r, int c) {
       int col = c+cc*M;
       if (row >=0 && row < R &&
 	  col >=0 && col < C && 
-	  accessible[row][col] && !visited[row][col])
-	dfs(row,col);
-    }
+	  accessible[row][col]){
+	cnt++;
+	if(!visited[row][col]) 
+	  dfs(row,col);
+      }
 
+      if (M!=N) {
+	row = r+rr*M;
+	col = c+cc*N;
+	if (row >=0 && row < R &&
+	    col >=0 && col < C && 
+	    accessible[row][col]){
+	  cnt++;
+	  if(!visited[row][col])
+	    dfs(row,col);
+	}
+      }
+    }
+  if (cnt%2)
+    odds++;
+  else
+    evens++;
 }
 
 void print_vec(vector<vector<int> >& v){
